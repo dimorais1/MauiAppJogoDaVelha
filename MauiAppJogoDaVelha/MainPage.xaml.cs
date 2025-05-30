42e8c119c5d33a134fa745716c3d4872fedfbe04
dimorais <bruno.araujo38@fatec.sp.gov.br>
2025-05-29 21:19:43 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppJogoDaVelha/MainPage.xaml.cs
@@ -0,0 +1,142 @@
+﻿namespace MauiAppJogoDaVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "X";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            btn.IsEnabled = false;
+
+            if (vez == "X")
+            {
+                btn.Text = "X";
+                vez = "O";
+            }
+            else
+            {
+                btn.Text = "O";
+                vez = "X";
+            }
+
+            /* Verificando se o X ganhou na 1ª linha */
+            if (btn00.Text == "X" && btn01.Text == "X" && btn02.Text == "X")
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X")
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn20.Text == "X" && btn21.Text == "X" && btn22.Text == "X")
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn00.Text == "X" && btn10.Text == "X" && btn20.Text == "X")
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn01.Text == "X" && btn11.Text == "X" && btn21.Text == "X")
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn02.Text == "X" && btn12.Text == "X" && btn22.Text == "X")
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn00.Text == "X" && btn11.Text == "X" && btn22.Text == "X")
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn02.Text == "X" && btn11.Text == "X" && btn20.Text == "X")
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn00.Text == "O" && btn01.Text == "O" && btn02.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn10.Text == "O" && btn11.Text == "O" && btn12.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn20.Text == "O" && btn21.Text == "O" && btn22.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn00.Text == "O" && btn10.Text == "O" && btn20.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn01.Text == "O" && btn11.Text == "O" && btn21.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn02.Text == "O" && btn12.Text == "O" && btn22.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn00.Text == "O" && btn11.Text == "O" && btn22.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+                Zerar();
+            }
+            else if (btn02.Text == "O" && btn11.Text == "O" && btn20.Text == "O")
+            {
+                DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+                Zerar();
+            }
+            /* Você precisará adicionar as outras verificações de vitória aqui,
+               usando os novos nomes dos botões:
+               btn10, btn11, btn12 para a segunda linha
+               btn20, btn21, btn22 para a terceira linha
+               e também para as colunas e diagonais. */
+
+        } // Fecha método
+
+        void Zerar()
+        {
+            btn00.Text = "";
+            btn01.Text = "";
+            btn02.Text = "";
+            btn10.Text = "";
+            btn11.Text = "";
+            btn12.Text = "";
+            btn20.Text = "";
+            btn21.Text = "";
+            btn22.Text = "";
+
+            btn00.IsEnabled = true;
+            btn01.IsEnabled = true;
+            btn02.IsEnabled = true;
+            btn10.IsEnabled = true;
+            btn11.IsEnabled = true;
+            btn12.IsEnabled = true;
+            btn20.IsEnabled = true;
+            btn21.IsEnabled = true;
+            btn22.IsEnabled = true;
+        }
+
+    } // Fecha Classe
+} // Fecha Namespace
